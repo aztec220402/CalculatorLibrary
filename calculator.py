3983d1e15b1e4dbedd27f0124b91910c17a5d832
Yash <aztec220402@gmail.com>
2021-10-12 13:48:06 +0530

    add multiply function

--- calculator.py
+++ calculator.py
@@ -4,8 +4,12 @@ Calculator library containing basic math operations.
 
 
 def add(num, second_num):
-    return num + second_num
+    return num+second_num
 
 
 def subtract(num, second_num):
     return num - second_num
+
+
+def multiply(num, second_num):
+    return num * second_num
